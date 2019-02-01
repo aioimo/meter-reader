a1e510a2b2620455a705eb3dc199b19006f0f832
Anthony Ioimo <anthonylioimo@gmail.com>
2019-02-01 11:23:32 +0100

    fix naming of Villages

--- routes/external.js
+++ routes/external.js
@@ -4,7 +4,7 @@ const router  = express.Router();
 
 /* Simulating an EXTERNAL API that my system relies upon  to determine village for each meter */
 router.get('/counter', (req, res, next) => {
-  res.json({id: req.query.id, village_name: "Village Number #" + req.query.id})
+  res.json({id: req.query.id, village_name: "Village Number " + req.query.id})
 });
 
 module.exports = router;
